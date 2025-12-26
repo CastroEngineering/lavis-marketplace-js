288d2321f9db352fd93194ca2305eaab814ac5fa
CastroEngineering <Joao-Marcos20@protonmail.com>
2025-12-26 13:57:34 -0500

    Fix typo ins express

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = required ('express');
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
@@ -13,6 +13,6 @@ res.send('<h1> Jewlery for those who write their own story</h>');
 app.listen(PORT, () =>
 {
 
-console.log('server is running ar http://localhost:${PORT}');
+console.log('server is running at http://localhost:${PORT}');
 
 });
