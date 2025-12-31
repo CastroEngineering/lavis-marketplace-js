f04f180a44cfd154165017aa7bdbc12e5d6c1b96
CastroEngineering <Joao-Marcos20@protonmail.com>
2025-12-30 23:53:00 -0500

    Fixed server.js syntax 3

--- api/server.js
+++ api/server.js
@@ -4,13 +4,13 @@ const app = express();
 
 
 app.use(express.static('public'));
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => 
 {
 
-res.render('index');
+    res.render('server');
 
 });
 
